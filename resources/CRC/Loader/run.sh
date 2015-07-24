eed73006652afb9109aa7d00cba18076557bc5fe
Jeremy R. Easton-Marks <j.r.eastonmarks@gmail.com>
2015-07-24 10:12:00 -0400

    Added XSD files, binding xml, and scripts to create classes for communication.

---
+++ resources/CRC/Loader/run.sh
@@ -0,0 +1,2 @@
+xjc -verbose -nv -p edu.harvard.hms.dbmi.bd2k.irct.ri.i2b2.message.crc.xml.loader *.xsd -b binding.xml 
+
---
+++ resources/CRC/PDO/run.sh
@@ -0,0 +1,1 @@
+xjc -verbose -nv -p edu.harvard.hms.dbmi.bd2k.irct.ri.i2b2.message.crc.xml.pdo CRC_PDO*.xsd -b binding.xml
